7a27f66b3070be141737678ed5ab32a8fa218581
Evan Tschuy <tschuy@osuosl.org>
2015-07-31 15:35:44 -0700

    Add checkProject tests

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,9 +1,35 @@
-// test/helpers.js
-
-var helper = require('../src/helpers');
+var helpers = require('../src/helpers');
 
 module.exports = function(expect) {
-    // This line exists to please the linters and tests
-    // Remove it when you write real tests.
-    expect(helper);
+    describe('checkProject', function() {
+        it('returns a project ID for proper slug', function(done) {
+            helpers.checkProject('ganeti-webmgr').then(function(project) {
+                expect(project).to.equal(1);
+                done();
+            });
+        });
+
+        it('throws when passed undefined', function(done) {
+            helpers.checkProject(undefined).then().catch(function(err) {
+                // this means that the time.slug was undefined
+                expect(err).to.equal(undefined);
+                done();
+            });
+        });
+
+        it('throws when passed a bad slug', function(done) {
+            helpers.checkProject('dogs').then().catch(function(err) {
+                err.sort();
+                expect(err).to.equal('dogs');
+                done();
+            });
+        });
+
+        it('throws when passed a null slug', function(done) {
+            helpers.checkProject(null).then().catch(function(err) {
+                expect(err).to.equal(null);
+                done();
+            });
+        });
+    });
 };

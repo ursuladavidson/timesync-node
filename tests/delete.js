ed151d33ef66465a6e8f073b348c6f6517865cb4
MaraJade <catbookgirl@gmail.com>
2015-08-05 12:38:12 -0700

    Changed fail project name to include more punctuation

--- tests/delete.js
+++ tests/delete.js
@@ -15,9 +15,9 @@ module.exports = function(expect) {
         });
 
         it('Fails if it receives an invalid project', function(done) {
-            deleter.deleteProject('Fail Project').then(function(project) {
+            deleter.deleteProject('Not.a-project!').then(function(project) {
                 //expect(res.statusCode).to.equal(400);
-                expect(project).to.be.a('Fail Project');
+                expect(project).to.be.a('Not.a-project!');
                 done();
             });
         });
